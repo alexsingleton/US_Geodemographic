ea6b65d5eaf88256e60ee99c06f86f14c3848f0a
aelissa <aelissa3388@gmail.com>
2020-10-27 15:48:55 +0000

    update

--- code/api.R
+++ code/api.R
@@ -18,17 +18,22 @@ con <- dbConnect(drv, dbname = "usgeodem",
 ##THIS FILE WAS MANUALLY EDITED IN EXCEL AND INCLUDES CODES INDICATING
 ##IF A VARIABLE SHOULD BE INCLUDED IN THE ANALYSIS.  THE COLUMN "NEW SET"
 ##IS THE FINAL VARIABLE LIST
-vars <- read.csv("usa_trt_varnames_051313.csv",stringsAsFactors = F)
+
+vars <- read.csv("data/usa_trt_varnames_051313.csv",stringsAsFactors = F)
 
 ##SELECTED VARIABLES
 in.vars <- as.character(vars[vars$new_set==0 & is.na(vars$new_set) == FALSE, "var"])
 vars <- vars[vars$var %in% in.vars,]
 
 ##get variables code
+
 vars$code<- sub('.*\\_','',vars$var)
 vars$code<-paste0(substring(vars$code,1,nchar(vars$code)-3),"_",substring(vars$code,nchar(vars$code)-2,nchar(vars$code)))
-v18 <- load_variables(2018, "acs5", cache = TRUE) #this is a reference to the variable names
-v18 <- v18[v18$name %in% vars$code,]
+denom<-paste0(unique(substr(vars[9:137,]$code,start=1, stop=6)),"_001")
+codes<-append(vars[4:nrow(vars),]$code,denom)  
+
+#v18 <- load_variables(2018, "acs5", cache = TRUE) #this is a reference to the variable names
+#v18 <- v18[v18$name %in% vars$code,]
 
 ##set api key and retrieve country codes
 census_api_key("376325c2baafb27e6ba5629590ba601eec2ab42c")
@@ -39,15 +44,15 @@ c<- detectCores() - 1
 cl <- makeCluster(c)
 registerDoParallel(cl)
 
-x<-foreach(i = 4:nrow(vars),.packages=c('purrr','dplyr','tidycensus')) %dopar%{
+x<-foreach(i = 1:length(codes),.packages=c('purrr','dplyr','tidycensus')) %dopar%{
   df <- map_df(us, function(x) {
-    get_acs(geography = "tract", variables = vars[i,]$code, 
+    get_acs(geography = "tract", variables = codes[i], 
             state = x, year = 2018)
   })
-  error <- paste0("moe_",vars[i,]$code)
+  error <- paste0("moe_",codes[i])
   df <-df %>%
     select(-variable) %>%
-    rename(!! vars[i,]$code := estimate,
+    rename(!! codes[i] := estimate,
            !! error := moe)
 }
 
@@ -56,5 +61,5 @@ x<-x %>%
   reduce(left_join,by=c("GEOID","NAME"))
 
 ##write data to db and as a csv
-dbWriteTable(con, "data", x, row.names=FALSE, append=TRUE)
+#dbWriteTable(con, "data", x, row.names=FALSE, append=TRUE)
 write.csv(x,"data.csv",row.names = F)
