3e0828fe925564acd406519005c189b6bdd0e96b
DESKTOP-0UQLQ5R\alexa <alexandersingleton@gmail.com>
2021-01-31 14:58:13 +0000

    index score calcs

--- code/5_Description.R
+++ code/5_Description.R
@@ -17,10 +17,71 @@ library(ggraph)
 library(viridis)
 library(summarytools)
 library(cluster)
+library(janitor)
 
 
 #Get Source Data
-readRDS("./data/data.rds")
+data <- readRDS("./data/data.rds")
+usa.trt.cl <- read_csv("Clusters.csv")
+vars_new <- readRDS("./data/vars_new.rds")
+
+
 
 
 # Append Clusters
+
+usa.trt.cl %<>%
+  left_join(data)
+
+#########################
+# Create Index Scores
+#########################
+
+X10 <- usa.trt.cl %>%
+  select(-c(GEOID,cluster,X45,X92,NAME)) %>%
+group_by(X10) %>% 
+  summarise_all(sum,na.rm = TRUE)
+
+
+X10 %<>%
+pivot_longer(
+  cols = starts_with("est_")) %>%
+pivot_wider(id_cols = "name",names_from = "X10")
+
+
+
+
+
+
+
+#Get base distribution
+base_pct <- X10 %>%
+  filter(name == "est_B01001_001") %>% # Total Population
+  adorn_percentages
+
+
+
+# Convert to percentages
+target_pct <- X10 %>%
+  filter(!str_detect(name, '_001')) %>%
+  adorn_percentages
+
+#Calculate index scores
+index_scores <- as_tibble((target_pct[,-1] / base_pct[rep(1:nrow(base_pct), nrow(target_pct)),-1]) * 100) %>% #calculate index score (target / base * 100)
+  mutate(UniqueID = sub("est_", "", target_pct$name)) %>%# append variable code
+  left_join(vars_new[,c("UniqueID")])
+
+# Get a list of ACS variables and append to index
+variables <- load_variables(2019,"acs5")
+
+index_scores %<>%
+  left_join(variables,by = c("UniqueID" = "name"))
+
+
+write_csv(index_scores,"grand_index.csv")
+
+
+
+
+
+
