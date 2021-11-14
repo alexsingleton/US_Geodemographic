42f115a321f22dd9b4d1baac7979a5019444e220
Alex Singleton <alexandersingleton@gmail.com>
2021-11-14 10:00:03 +0000

    github setup

--- code/0_Setup.R
+++ code/0_Setup.R
@@ -1,30 +1,4 @@
-#Setup
+usethis::use_git_config(user.name = "Alex Singleton", user.email = "alexandersingleton@gmail.com")
+usethis::create_github_token()
 
-#Terminal - Setup the Python Version
-pipenv --python 3.7
-
-#R - Setup the virtual environment
-
-#venv <- system("pipenv --venv", inter = TRUE)
-#reticulate::use_virtualenv(venv, required = TRUE)
-
-#Install Tensorflow & Kerras
-
-#install.packages("tensorflow")
-#install.packages("keras")
-#install_keras(tensorflow = "gpu")
-
-
-wget 'https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh'
-bash Anaconda3-2021.05-Linux-x86_64.sh
-
-
-
-library(devtools)
-devtools::install_github("h2oai/h2o4gpu", subdir = "src/interface_r")
-
-#Setup Virtual Environment
-reticulate::use_virtualenv("/home/rstudio/anaconda3/envs/h2o4gpuenv")
-
-
-library(h2o4gpu)
+gitcreds::gitcreds_set()
